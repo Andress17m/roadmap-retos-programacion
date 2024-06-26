58d6d71b368a5bf58e3a9792ca6b52711ade5706
Diego De Avila <andres1630d@gmail.com>
2024-06-25 23:03:37 -0500

    #00-python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DiegoDeAvila.py
@@ -0,0 +1,26 @@
+#https://www.python.org/
+
+#Comentario en una línea 
+
+'''
+Utilizamos las comillas de esta 
+Manera para poner escribir 
+Comentarios en varias lineas
+
+'''
+
+#Variables
+
+name = "Diego"
+
+PI = 3.14 #Variable const por convención
+
+NumInt = 18
+NumFloat = 18.6
+
+myBool = True
+myBool = False
+
+mystring = "Cadena de txt"
+
+print("¡Hola, Python!")
